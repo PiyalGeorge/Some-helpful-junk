d846caf4b0defaa33259709d4fd65f595c62bbdc
PiyalGeorge <piyalgeorge@gmail.com>
2019-07-16 14:34:09 +0530

    Update count-files.py

--- python-codes/count-files.py
+++ python-codes/count-files.py
@@ -4,7 +4,7 @@ This code is to count the files in a directory/folder
 
 import os
 
-directory = "/home/vkchcp0013/Documents/Helpful-codes" # Enter directory 
+directory = "/home/Documents/Helpful-codes" # Enter directory 
 
 number_files = len(os.listdir(directory)) 
 print("The total number of files:", number_files)
