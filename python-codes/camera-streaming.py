20febec0d44d320ee405be8862e644ecad078447
PiyalGeorge <piyalgeorge@gmail.com>
2020-05-13 17:49:44 +0530

    camera streaming using opencv
    simply camera streaming using opencv for immediate use

---
+++ python-codes/camera-streaming.py
@@ -0,0 +1,20 @@
+import cv2
+
+
+cv2.namedWindow("preview")
+vc = cv2.VideoCapture(0)
+
+if vc.isOpened(): # try to get the first frame
+    rval, frame = vc.read()
+else:
+    rval = False
+
+while rval:
+    frame = cv2.flip(frame, 1)
+    cv2.imshow("preview", frame)
+    rval, frame = vc.read()
+    key = cv2.waitKey(20)
+    if key == 27: # exit on ESC
+        break
+
+cv2.destroyWindow("preview")
